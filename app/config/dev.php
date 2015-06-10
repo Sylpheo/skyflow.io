781d43cde6a239903804e072a7890db2327f99f4
elodie <elodie@Serveur>
2015-06-10 11:14:30 +0200

    prod

--- app/config/dev.php
+++ app/config/dev.php
@@ -1,5 +1,5 @@
 <?php
-/*$app['db.options'] = array(
+$app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
     'host'     => '127.0.0.1',  // Mandatory for PHPUnit testing
@@ -7,16 +7,6 @@
     'dbname'   => 'exacttarget',
     'user'     => 'root',
     'password' => 'swiffer',
-);*/
-
-$app['db.options'] = array(
-    'driver'   => 'pdo_pgsql',
-    'charset'  => 'utf8',
-    'host'     => 'ec2-54-83-57-86.compute-1.amazonaws.com',
-    'port'     => '5432',
-    'dbname'   => 'd70b123ck17v74',
-    'user'     => 'yasrxhlztiwpnq',
-    'password' => 'YT5kh8cYoPq4e9Xa2i2cWWz7rs',
 );
 
 // enable the debug mode
