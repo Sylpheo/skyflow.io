2c97a26519be094b4e3c3136ef81b40cb73aa160
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2015-08-24 14:43:47 +0200

    Move db configurations files from "skyflow.io/db" to "skyflow.io/app/db"

--- app/config/dev.php
+++ app/config/dev.php
@@ -4,7 +4,7 @@
  * Application configuration for the development environment.
  */
 
-$app['db.options'] = include __DIR__ . '/../../db/dev.php';
+$app['db.options'] = include __DIR__ . '/../db/dev.php';
 
 $app['debug'] = true;
 $app['monolog.level'] = 'INFO';
--- app/config/prod.php
+++ app/config/prod.php
@@ -4,7 +4,7 @@
  * Application configuration for the production environment.
  */
 
-$app['db.options'] = include __DIR__ . '/../../db/prod.php';
+$app['db.options'] = include __DIR__ . '/../db/prod.php';
 
 //$app['debug'] = true;
 $app['monolog.level'] = 'WARNING';
