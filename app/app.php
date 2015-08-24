8d8fa4fde1ec2f1228c6b01c2f10221a72afa7f0
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2015-08-24 10:08:35 +0200

    Reorganize app/app.php

--- app/app.php
+++ app/app.php
@@ -17,11 +17,38 @@ require_once __DIR__ . '/config/dev.php';
 //require_once __DIR__ . '/config.prod.php';
 require_once __DIR__ . '/../app/routes.php';
 
+// ========== Error Handlers ==========
+
 ErrorHandler::register();
 ExceptionHandler::register();
 
-// Register service providers
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->error(function (\Exception $e, $code) use ($app) {
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = "Something went wrong.";
+    }
+
+    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
+
+// ========== Silex Service Providers ==========
+
+$app->register(new Silex\Provider\DoctrineServiceProvider());
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/silex.log',
+    'monolog.name' => 'silex',
+    'monolog.level' => $app['monolog.level']
+));
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
@@ -30,13 +57,13 @@ $app['twig'] = $app->share($app->extend('twig', function(Twig_Environment $twig,
     $twig->addExtension(new Twig_Extensions_Extension_Text());
     return $twig;
 }));
+
 $app->register(new Silex\Provider\TranslationServiceProvider());
-$app->register(new Silex\Provider\DoctrineServiceProvider());
 
-
-
-$app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new FormServiceProvider());
+
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
         'secured' => array(
@@ -51,47 +78,21 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 
-
-$app->register(new FormServiceProvider());
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../var/logs/silex.log',
-    'monolog.name' => 'silex',
-    'monolog.level' => $app['monolog.level']
-));
-
-
-// Register services
-$app['dao.event'] = $app->share(function ($app) {
-    return new skyflow\DAO\EventDAO($app['db']);
-});
-
-$app['exacttarget'] = $app->share(function($app) {
-    if ($app['security']->isGranted('IS_AUTHENTICATED_FULLY')) {
-        return skyflow\Service\ExactTarget::login($app);
-    }else{
-        return skyflow\Service\ExactTarget::loginByApi($app);
-    }
-});
+// ========== Custom Services ==========
 
 $app['generatetoken'] = $app->share(function($app) {
     $generate = new skyflow\Service\GenerateToken();
     return $generate;
 });
 
-$app['wave'] = $app->share(function($app) {
-    if($app['security']->isGranted('IS_AUTHENTICATED_FULLY')) {
-        return skyflow\Service\Wave::login($app);
-    }else{
-        return skyflow\Service\Wave::loginByApi($app);
-    }
-});
+// ========== DAO ==========
 
-$app['salesforce'] = $app->share(function(){
-    return new skyflow\Service\Salesforce();
-});
-//DAO
 $app['dao.user'] = $app->share(function ($app) {
     return new skyflow\DAO\UsersDAO($app['db']);
+});
+
+$app['dao.event'] = $app->share(function ($app) {
+    return new skyflow\DAO\EventDAO($app['db']);
 });
 
 $app['dao.flow'] = $app->share(function ($app) {
@@ -109,24 +110,30 @@ $app['dao.wave_request'] = $app->share(function ($app){
     return new skyflow\DAO\Wave_requestDAO($app['db']);
 });
 
-//Register Flows
+// ========== Flows ==========
+
 $app['flow_mail_remerciements'] = $app->share(function ($app){
     return new skyflow\Flows\Flow_mail_remerciements($app);
 });
 
+// ========== Addons ==========
 
-
-// Register error handler
-$app->error(function (\Exception $e, $code) use ($app) {
-    switch ($code) {
-        case 403:
-            $message = 'Access denied.';
-            break;
-        case 404:
-            $message = 'The requested resource could not be found.';
-            break;
-        default:
-            $message = "Something went wrong.";
+$app['exacttarget'] = $app->share(function($app) {
+    if ($app['security']->isGranted('IS_AUTHENTICATED_FULLY')) {
+        return skyflow\Service\ExactTarget::login($app);
+    }else{
+        return skyflow\Service\ExactTarget::loginByApi($app);
     }
-    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
+
+$app['salesforce'] = $app->share(function(){
+    return new skyflow\Service\Salesforce();
+});
+
+$app['wave'] = $app->share(function($app) {
+    if($app['security']->isGranted('IS_AUTHENTICATED_FULLY')) {
+        return skyflow\Service\Wave::login($app);
+    }else{
+        return skyflow\Service\Wave::loginByApi($app);
+    }
 });
