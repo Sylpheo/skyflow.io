fef8eab05c38c4f736d099cfb399161658dbe054
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2015-08-26 23:33:51 +0200

    Revert "Trying to debug parsing of DATABASE_URL"
    This reverts commit b3b04c704f0624659cb8a444db43fa55a700ea36.

--- app/db.php
+++ app/db.php
@@ -8,9 +8,7 @@
 
 $db = parse_url(getenv('DATABASE_URL'));
 
-var_dump($db);
-
-$db_config = array(
+return array(
     'driver'   => 'pdo_pgsql',
     'charset'  => 'utf8',
     'host'     => $db['host'],
@@ -19,7 +17,3 @@ $db_config = array(
     'password' => $db['pass'],
     'dbname'   => substr($db['path'], 1)
 );
-
-var_dump($db_config);
-
-return $db_config;
