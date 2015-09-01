a1bdb069810e06fa36c3c9fca3d19bb0861d498f
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2015-09-01 16:00:14 +0200

    Fix missing access_token after setting salesforce and wave credentials
    When adding salesforce credentials or wave credentials, do the authorize if the access_token for salesforce/wave is empty even if client_id, client_secret, is_sandbox are not changed.

--- src/Salesforce/Controller/SalesforceOAuthUserController.php
+++ src/Salesforce/Controller/SalesforceOAuthUserController.php
@@ -26,9 +26,12 @@ class SalesforceOAuthUserController extends OAuthUserController
         ) {
             $data = $this->getCredentialsForm()->getData();
 
-            if ($this->getUser()->getClientId() !== $data['client_id']
-                || $this->getUser()->getClientSecret() !== $data['client_secret']
-                || $this->getUser()->getIsSandbox() !== $data['is_sandbox']
+            if (empty($this->getUser()->getAccessToken())
+                || (
+                    $this->getUser()->getClientId() !== $data['client_id']
+                    || $this->getUser()->getClientSecret() !== $data['client_secret']
+                    || $this->getUser()->getIsSandbox() !== $data['is_sandbox']
+                )
             ) {
                 $this->getUser()->setClientId($data['client_id']);
                 $this->getUser()->setClientSecret($data['client_secret']);
