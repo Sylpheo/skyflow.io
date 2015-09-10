374c669292bd306161956def3b603dce60e7b996
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2015-09-10 11:49:45 +0200

    Update TestFlow with new services return values

--- src/Skyflow/Flow/TestFlow.php
+++ src/Skyflow/Flow/TestFlow.php
@@ -22,18 +22,70 @@ class TestFlow extends AbstractFlow
 
     public function run()
     {
-        // Send a SOQL request to Salesforce.
-        // Be sure the Salesforce addon is setup with client id and client secret
-        // or this will not work.
-        $resultSalesforce = $this->getSalesforce()->getData()->query('SELECT Name FROM Account LIMIT 1');
+        //return $this->runSalesforce();
+        $this->runWave();
+    }
 
-        // Send a SAQL request to Wave
-        // Be sure the Wave addon is setup with client id and client secret or
-        // this will not work.
-        return $this->getWave()->getData()->query(
+    public function runSalesforce()
+    {
+        // Send a SOQL query to Salesforce.
+        $records = $this->get('salesforce.data')->query(
+            "SELECT Id FROM Account WHERE Name = 'Test_TestFlow'"
+        );
+
+        if (count($records) === 0) {
+            // Create a SObject in Salesforce.
+            $id = $this->get('salesforce.data.sobjects')->create('Account', array(
+                'Name' => 'Test_TestFlow'
+            ));
+        } else {
+            $id = $records[0]['Id'];
+        }
+
+        // Update an SObject in Salesforce.
+        $this->get('salesforce.data.sobjects')->update('Account', $id, array(
+            'Name' => 'Test_TestFlow_updated'
+        ));
+
+        // Delete an SObject in Salesforce.
+        $this->get('salesforce.data.sobjects')->delete('Account', $id);
+    }
+
+    public function runWave()
+    {
+        $this->get('wave.data')->query(
             'q = load "0FbB00000005KPEKA2/0FcB00000005W4tKAE";'
             . 'q = filter q by Email in ["e.lodie62@hotmail.fr"];'
             . 'q = foreach q generate FirstName as FirstName, LastName as LastName'
         );
+
+        $dataset = $this->get('wave.externaldata')->create(
+            'Multiplication',
+            array(
+                'edgemartAlias' => 'Multiplication',
+                'format' => 'Csv',
+                'operation' => 'Overwrite',
+                'notificationSent' => 'Warnings',
+                'notificationEmail' => 'adrien.desfourneaux@sylpheo.com',
+                'description' => 'Multiplication table'
+            )
+        );
+
+        // 1 row for column headers
+        $dataset->appendCsvLine(array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        // 10 rows multiplication table.
+        for ($i = 1; $i <= 10; $i++) {
+            $row = array();
+
+            // 10 columns multiplication table
+            for ($j = 1; $j <= 10; $j++) {
+                $row[$j] = $i * $j;
+            }
+
+            $dataset->appendCsvLine($row);
+        }
+
+        $dataset->process();
     }
 }
