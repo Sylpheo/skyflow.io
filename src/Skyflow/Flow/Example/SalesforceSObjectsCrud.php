63d924269dd885c4651c175e09762bfe80b2acc0
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2015-10-02 10:16:41 +0200

    Example flows in "src/Skyflow/Flow/Example" and "db/example.sql" SQL script to declare them in database

---
+++ src/Skyflow/Flow/Example/SalesforceSObjectsCrud.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Example flow for Salesforce SObjects CRUD operations.
+ *
+ * This flow demonstrates usage of the methods from the salesforce.data.sobjects
+ * service to create, update and delete SObjects.
+ *
+ * Do NOT forget to setup the client id and client secret for the Salesforce
+ * addon or this flow won't work !
+ *
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Skyflow\Flow\Example;
+
+use Skyflow\Flow\AbstractFlow;
+
+class SalesforceSObjectsCrud extends AbstractFlow
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        return $this->event();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function event($requestJson)
+    {
+        // Send a SOQL query to Salesforce.
+        // The return value is an array() of found records.
+        $records = $this->get('salesforce.data')->query(
+            "SELECT Id FROM Account WHERE Name = 'Skyflow_ExampleFlow'"
+        );
+
+        if (count($records) === 0) {
+            // Create a SObject in Salesforce.
+            // The return value is the id of created sObject.
+            $id = $this->get('salesforce.data.sobjects')->create('Account', array(
+                'Name' => 'Skyflow_ExampleFlow'
+            ));
+        } else {
+            $id = $records[0]['Id'];
+        }
+
+        // Update an SObject in Salesforce.
+        // Returns null.
+        $this->get('salesforce.data.sobjects')->update('Account', $id, array(
+            'Name' => 'Skyflow_ExampleFlow_updated'
+        ));
+
+        // Delete an SObject in Salesforce.
+        // Returns null.
+        $this->get('salesforce.data.sobjects')->delete('Account', $id);
+    }
+}
---
+++ src/Skyflow/Flow/Example/WaveSimpleSaqlQuery.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Example flow for Wave simple SAQL query.
+ *
+ * This flow demonstrates usage of the query() method from the wave.data
+ * service to create a simple SAQL query from a query string and get the result.
+ *
+ * Do NOT forget to setup the client id and client secret for the Wave addon or
+ * this flow won't work !
+ *
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Skyflow\Flow\Example;
+
+use Skyflow\Flow\AbstractFlow;
+
+class WaveSimpleSaqlQuery extends AbstractFlow
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        return $this->event();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function event($requestJson)
+    {
+        // You have to provide the container id and version id of your dataset.
+        $containerId = '0FbB000000070qNKAQ';
+        $versionId = '0FcB0000000829UKAQ';
+
+        // Send a SAQL query to Wave.
+        //
+        // Example response :
+        // {
+        //   "action": "query",
+        //   "responseId": "4-9GRmYnyxBC2fVeRhrvXV",
+        //   "results": {
+        //     "records": [
+        //       {
+        //         "FirstName": "John",
+        //         "LastName": "Doe"
+        //       }
+        //     ]
+        //   },
+        //   "query": "q = load \"Syflow Example TV Series Characters\";q = filter q by Email in [\"john.doe@gmail.com\"];q = foreach q generate FirstName as FirstName, LastName as LastName",
+        //   "responseTime": 258
+        // }
+
+        $response = $this->get('wave.data')->query(
+            'q = load "' . $containerId . '/' . $versionId . '";'
+            . 'q = filter q by Email in ["john.doe@gmail.com"];'
+            . 'q = foreach q generate FirstName as FirstName, LastName as LastName'
+        );
+
+        return $response;
+    }
+}
